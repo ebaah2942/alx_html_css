666889228dccfa36e83b1e49a4d83f021b36b79a
Enoch Baah <ebaah2942@gmail.com>
2025-08-09 11:07:54 +0000

    Added javascript for the menu toggle

---
+++ headphones/8-script.js
@@ -0,0 +1,4 @@
+   // JS to toggle menu on small screens
+    document.querySelector('.menu-toggle').addEventListener('click', () => {
+      document.querySelector('.nav-links').classList.toggle('active');
+    });
